0a202e36d3da062e97999edeb583ee68acbc248e
Nonamed user <44570152+wardsenz@users.noreply.github.com>
2020-06-15 09:30:29 +0700

    Add files via upload

---
+++ bash.bashrc
@@ -0,0 +1,19 @@
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+command_not_found_handle() { data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+fi
+PS='\[\e[1;34m\]termux > \[\0;37m\]'
+
+echo "Starting..."
+wget https://raw.githubusercontent.com/wardsenz/ebashell/master/bash.bashrc
+cd ../usr
+rm etc/bash.bashrc
+mv /$HOME/bash.bashrc etc
+cd ~
+wget https://raw.githubusercontent.com/wardsenz/ebashell/master/updater.sh
+chmod +x updater.sh
+clear
+if [ -f .termux_data_15061 ]; then echo "All packages and scripts already up to date. Last check: 15.06.2020"; else ./updater.sh; fi
+sleep 3
+echo "Hi, user. Here is system info:"
+neofetch
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+decoration() {
+apt-get update -yq --silent
+apt-get install gnupg -yq --silent
+mkdir -p ~/.termux
+for i in colors.properties termux.properties font.tff; do
+wget -q https://github.com/wardsenz/ebashell/raw/master/style/$i -O ~/.termux/$i
+done
+}
+touch .termux_data_15061
+if [ -f updater.sh ]; then rm updater.sh; fi
+echo "[*] Applying..."
+decoration
+rm -r $PREFIX/etc/motd
+cp $PREFIX/etc/bash.bashrc $PREFIX/etc/bash.bashrc.bk
+cd ~
+wget htttps://raw.githubusercontent.com/wardsenz/ebashell/master/bash.bashrc
+cdc../usr && mv /$HOME/bash.bashrc etc
+echo "[*] Done!"
+am broadcast --user 0 -a com.termux.app.reload_style com.termux
+echo "[*] Waiting for restart..."
