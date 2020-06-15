e870a43371fab5102af6bfa4f9056179c9df0287
Nonamed user <44570152+wardsenz@users.noreply.github.com>
2020-06-15 08:52:06 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,8 +1,8 @@
-cd ~
+cd
 echo "Начинаю процесс обновления...."
 if [ -f install.sh ]; then rm install.sh; fi
 echo "Проверяю, установлен ли пакет wget..." && sleep 5
 if dpkg -s wget; then echo "wget - связь есть. Секунду..." && sleep 5; else echo "Пакет wget отсутствует. Устанавливаю wget..." && sleep 5 && pkg install wget -y > /dev/null; fi
-wget https://raw.githubusercontent.com/wardsenz/ebashell/master/install.sh > /dev/null
+wget https://raw.githubusercontent.com/wardsenz/ebashell/master/install.sh
 chmod +x install.sh
 ./install.sh
