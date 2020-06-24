548bacba32dee352e41592e0df03c52fbf868718
unnamed <44570152+wardsenz@users.noreply.github.com>
2020-06-25 05:27:56 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -3,6 +3,6 @@ cd ~
 echo "Updating..."
 if [ -f install.sh ]; then rm -f install.sh; fi
 echo "Please wait..." && sleep 3
-wget https://raw.githubusercontent.com/wardsenz/ebashell/master/install.sh
+wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/install.sh
 chmod +x install.sh
 sh install.sh
