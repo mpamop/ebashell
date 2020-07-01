ca1865f6e103a8e59c867efd5cb08b19fe78d15f
wardsenz/cli <hydrugz-noreply@yandex.ru>
2020-07-01 21:11:09 +0700

    updater.sh fix

--- updater.sh
+++ updater.sh
@@ -4,10 +4,9 @@ update() {
 	if [ "$(curl -I --silent google.com)" ]
 	then
 		printf "Обновление...\n"
-		RAW="https://raw.githubusercontent.com/wardsenz/ebashell/master/"
 		cd $PREFIX/share/ebashell
 		if [ -f install.sh ]; then rm -f install.sh; fi
-			if [ $(wget -q ""$RAW"install.sh") ]; then
+			if [ $(wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/install.sh) ]; then
 			chmod +x install.sh && bash install.sh
 			else
 			printf "Ошибка при скачивании обновления.\n Возможные причины: Неполадки с соединением с GitHub и/или отсутствует пакет wget.\n"
