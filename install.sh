be223a5dea06fb2860f6ddd1e3466fc80b589314
wardsenz <hydrugz-noreply@yandex.ru>
2020-07-07 19:53:08 +0700

    Добавлен minfetch/улучшен код

--- install.sh
+++ install.sh
@@ -14,19 +14,53 @@ done
 }
 echo "[*] Установка..."
 decoration
-if [ -f $PREFIX/etc/motd ]; then rm -r $PREFIX/etc/motd; fi
-cd $PREFIX/bin
-if [ -f ebashell-update ]; then rm -f ebashell-update; fi
-wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/updater.sh && mv updater.sh ebashell-update && chmod +x ebashell-update
-cd $PREFIX/etc && rm bash.bashrc && wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/bash.bashrc
-cd $PREFIX
-if [ -d share/ebashell ]; then rm -rf share/ebashell; fi
-cd share
-git clone https://github.com/wardsenz/ebashell > /dev/null
-cd ebashell && touch data.json
-echo "Последнее обновление репозитория: 01.07.2020" > data.json
-echo "Дата установки/Последней проверки: $(date)" >> data.json
-cd $HOME
-echo "[*] Успешно!"
-am broadcast --user 0 -a com.termux.app.reload_style com.termux > /dev/null
-echo "[*] Пожалуйста, перезапустите Termux..."
+	if [ -f $PREFIX/etc/motd ]; then
+		rm -r $PREFIX/etc/motd;
+	fi
+
+	cd $PREFIX/bin
+
+	updatefile() {
+		if [ -f ebashell-update ]; then
+			rm -f ebashell-update;
+		fi
+		wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/updater.sh
+		mv updater.sh ebashell-update
+		chmod +x ebashell-update
+	}
+update_updater
+# o_O
+	update_minfetch() {
+		if [ -f minfetch ]; then
+			rm -f minfetch;
+		fi
+		wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/minfetch.sh
+		mv minfetch.sh minfetch
+		chmod +x minfetch
+	}
+update_minfetch
+
+	update_bashrc() {
+		cd $PREFIX/etc
+		rm bash.bashrc
+		wget -q https://raw.githubusercontent.com/wardsenz/ebashell/master/bash.bashrc
+		}
+update_bashrc
+
+	update_clone() {
+		cd $PREFIX
+		if [ -d share/ebashell ]; then
+			rm -rf share/ebashell;
+		fi
+		cd share
+		git clone https://github.com/wardsenz/ebashell > /dev/null 2>&1
+		cd ebashell
+		touch data.json
+		curl https://api.github.com/repos/wardsenz/ebashell/commits/master 2>&1 | grep '"date"' | tr -d ' :"date' | tail -n 1 > data.json
+		echo "Дата установки/последней проверки: $(date)" >> data.json
+		cd $HOME
+		}
+
+	echo "[*] Успешно!"
+	am broadcast --user 0 -a com.termux.app.reload_style com.termux > /dev/null
+	echo "[*] Пожалуйста, перезапустите Termux..."
